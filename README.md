8873ea3ebf021cecc51230bf789090bdb5434dde
KHAAdotPK <spysee.pk@gmail.com>
2024-10-13 13:51:55 +0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# read_write_weights
-Macros to read trained weights from files and store them in memory
+# Weight Filling Macros Library
+
+This library provides macros to read trained weights from files and store them in memory using `Numcy::Collective<double>` structures. The current macros `REAS_W1` and `READ_W2` are designed to read the respective weights and store them in a new instance of a composite data structure.
+
+## Macros
+
+### `READ_W1(p, w1)`
+
+This macro reads weight values from a file using an instance of the tokenizer and fills the provided `w1` instance of `Collective<double>` with the weight data.
+
+- **Parameters:**
+  - `p`: An instance of the tokenizer that reads the weight data file.
+  - `w1`: An instance of `Collective<double>` that stores the weights.
+
+- **Functionality:**
+  - Resets the tokenizer to read lines and tokens from the weight file.
+  - Initializes `w1` with dimensions based on the number of lines and tokens.
+  - Fills the weights into `w1` by iterating through lines and tokens of the file.
+
+### `READ_W2(p, w2)`
+
+This macro works similarly to `READ_W1`, but with a different orientation for how the weights are filled into the `w2` instance of `Collective<double>`.
+
+- **Parameters:**
+  - `p`: An instance of the tokenizer that reads the weight data file.
+  - `w2`: An instance of `Collective<double>` that stores the weights.
+
+- **Functionality:**
+  - Resets the tokenizer to read lines and tokens from the weight file.
+  - Initializes `w2` with dimensions based on the number of lines and tokens.
+  - Fills the weights into `w2` by iterating through lines and tokens of the file, in a column-wise manner.
+
+## Upcoming Macros
+
+~~In the near future, this library will include additional macros for writing the trained weights back to files. These macros will make it easier to save and load weight data for model deployment~~.
+
+## Example Usage
+
+```cpp
+#ifdef GRAMMAR_END_OF_TOKEN_MARKER
+#undef GRAMMAR_END_OF_TOKEN_MARKER
+#endif
+#ifdef GRAMMAR_END_OF_LINE_MARKER
+#undef GRAMMAR_END_OF_LINE_MARKER
+#endif
+
+#define GRAMMAR_END_OF_TOKEN_MARKER ' '
+#define GRAMMAR_END_OF_LINE_MARKER '\n'
+
+/*
+    Dependencies
+    - csv
+    - Numcy
+    - sundry
+ */
+#include "../lib/csv/parser.hh"
+#include "../lib/Numcy/header.hh"
+#include "../lib/sundry/cooked_read_new.hh"
+#include "../lib/sundry/cooked_write_new.hh"
+
+#include "../lib/read_write_weights/header.hh"
+
+int main(int argc, char* argv[])
+{
+    cc_tokenizer::String<char> w1trained = cc_tokenizer::cooked_read<char>(cc_tokenizer::String<char>("w1trained.txt"));
+    cc_tokenizer::String<char> w2trained = cc_tokenizer::cooked_read<char>(cc_tokenizer::String<char>("w2trained.txt"));
+
+    Collective<double> W1;
+    Collective<double> W2;
+
+    cc_tokenizer::csv_parser<cc_tokenizer::String<char>, char> w1trainedParser(w1trained);  
+    cc_tokenizer::csv_parser<cc_tokenizer::String<char>, char> w2trainedParser(w2trained);
+
+    READ_W1(w1trainedParser, W1);
+    READ_W2(w2trainedParser, W2);
+
+    // Use w1 and w2 for further processing
+
+    return 0;
+}
+```
---
+++ header.hh
@@ -0,0 +1,160 @@
+/*
+    lib/read_write_weights/header.hh
+    Q@khaa.pk
+ */
+
+/*
+ * Temporary Inclusion of hyper-parameters.hh
+ * 
+ * This inclusion serves as an ad-hoc solution to address the current need for 
+ * hyper-parameters in this implementation, but this approach may not be 
+ * the best long-term solution.
+ */
+#ifndef SKIP_GRAM_HYPER_PARAMETERS_HEADER_HH
+#include "hyper-parameters.hh"
+#endif
+
+#ifndef READ_WEIGHTS_HEADER_HEADER_HH
+#define READ_WEIGHTS_HEADER_HEADER_HH
+
+/*
+    @p,  instance of tokenizer
+    @w1, instance of Collective<double>
+ */
+#define  READ_W1(p, w1)\
+{\
+    p.reset(LINES);\
+    p.reset(TOKENS);\
+    /* Adjusting line and token counts to account for an off-by-one error in the parser.\
+    The parser seems to return one additional line and one extra token than expected,\
+    so we subtract 1 from the total counts to ensure we process only the actual lines and tokens */\
+    cc_tokenizer::string_character_traits<char>::size_type r = p.get_total_number_of_lines() - 1;\
+    p.go_to_next_line();\
+    /* Adjusting line and token counts to account for an off-by-one error in the parser.\
+    The parser seems to return one additional line and one extra token than expected,\
+    so we subtract 1 from the total counts to ensure we process only the actual lines and tokens */\
+    cc_tokenizer::string_character_traits<char>::size_type c = p.get_total_number_of_tokens() - 1;\
+    p.reset(LINES);\
+    try\
+    {\
+        w1 = Numcy::zeros(DIMENSIONS{c, r, NULL, NULL});\
+    }\
+    catch (ala_exception& e)\
+    {\
+        std::cout<< e.what() << std::endl;\
+    }\
+    cc_tokenizer::string_character_traits<char>::size_type i = 0;\
+    while (p.go_to_next_line() != cc_tokenizer::string_character_traits<char>::eof())\
+    {\
+        cc_tokenizer::string_character_traits<char>::size_type j = 0;\
+        p.get_token_by_number(1);\
+        while (p.go_to_next_token() != cc_tokenizer::string_character_traits<char>::eof())\
+        {\
+            w1[i*c + j] = atof(p.get_current_token().c_str());\
+            j = j + 1;\
+        }\
+        i = i + 1; \
+    }\
+}\
+
+/*
+    @p,  instance of tokenizer
+    @w1, instance of Collective<double>    
+ */
+#define READ_W2(p, w2)\
+{\
+    p.reset(LINES);\
+    p.reset(TOKENS);\
+    /* Adjusting line and token counts to account for an off-by-one error in the parser.\
+    The parser seems to return one additional line and one extra token than expected,\
+    so we subtract 1 from the total counts to ensure we process only the actual lines and tokens */\
+    cc_tokenizer::string_character_traits<char>::size_type r = p.get_total_number_of_lines() - 1;\
+    p.go_to_next_line();\
+    /* Adjusting line and token counts to account for an off-by-one error in the parser.\
+    The parser seems to return one additional line and one extra token than expected,\
+    so we subtract 1 from the total counts to ensure we process only the actual lines and tokens */\
+    cc_tokenizer::string_character_traits<char>::size_type c = p.get_total_number_of_tokens() - 1;\
+    p.reset(LINES);\
+    try\
+    {\
+        w2 = Numcy::zeros(DIMENSIONS{r, c, NULL, NULL});\
+    }\
+    catch (ala_exception& e)\
+    {\
+        std::cout<< e.what() << std::endl;\
+    }\
+    cc_tokenizer::string_character_traits<char>::size_type i = 0;\
+    while (p.go_to_next_line() != cc_tokenizer::string_character_traits<char>::eof())\
+    {\
+        cc_tokenizer::string_character_traits<char>::size_type j = 0;\
+        p.get_token_by_number(1);\
+        while (p.go_to_next_token() != cc_tokenizer::string_character_traits<char>::eof())\
+        {\
+            w2[j*c + i] = atof(p.get_current_token().c_str());\
+            j = j + 1;\
+        }\
+        i = i + 1; \
+    }\
+}\
+
+/*    
+    @w1, instance of Collective<double>, these weights are written to @f
+    @f,  name of file where @w1 are written to, instance of class String
+    @v,  vocabulary, instance of class corpus
+ */
+#define WRITE_W1(w1, f, v)\
+{\
+for (cc_tokenizer::string_character_traits<char>::size_type i = 0; i < v.numberOfUniqueTokens(); i++)\
+    {\
+        cc_tokenizer::String<char> line;\
+        line = line + v[i + INDEX_ORIGINATES_AT_VALUE] + cc_tokenizer::String<char>(" ");\
+        cc_tokenizer::string_character_traits<char>::size_type j = 0;\
+        for (; j < SKIP_GRAM_EMBEDDNG_VECTOR_SIZE;)\
+        {\
+            cc_tokenizer::String<char> num(w1[i*SKIP_GRAM_EMBEDDNG_VECTOR_SIZE + j]);\
+            j++;\
+            if (j < SKIP_GRAM_EMBEDDNG_VECTOR_SIZE)\
+            {\
+                line = line + num + cc_tokenizer::String<char>(" ");\
+            }\
+            else\
+            {\
+                line = line + num;\
+            }\
+        }\
+        line = line + cc_tokenizer::String<char>("\n");\
+        cc_tokenizer::cooked_write(f, line);\
+    }\
+}\
+
+/*    
+    @w2, instance of Collective<double>, these weights are written to @f
+    @f,  name of file where @w1 are written to, instance of class String
+    @v,  vocabulary, instance of class corpus
+ */
+#define WRITE_W2(w2, f, v)\
+{\
+    for (cc_tokenizer::string_character_traits<char>::size_type i = 0; i < v.numberOfUniqueTokens(); i++)\
+    {\
+        cc_tokenizer::String<char> line;\
+        line = line + v[i + INDEX_ORIGINATES_AT_VALUE] + cc_tokenizer::String<char>(" ");\
+        cc_tokenizer::string_character_traits<char>::size_type j = 0;\
+        for (; j < SKIP_GRAM_EMBEDDNG_VECTOR_SIZE;)\
+        {\
+            cc_tokenizer::String<char> num(w2[j*SKIP_GRAM_EMBEDDNG_VECTOR_SIZE + i]);\
+            j++;\
+            if (j < SKIP_GRAM_EMBEDDNG_VECTOR_SIZE)\
+            {\
+                line = line + num + cc_tokenizer::String<char>(" ");\
+            }\
+            else\
+            {\
+                line = line + num;\
+            }\
+        }\
+        line = line + cc_tokenizer::String<char>("\n");\
+        cc_tokenizer::cooked_write(f, line);\
+    }\
+}\
+
+#endif
